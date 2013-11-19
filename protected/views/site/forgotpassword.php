00ecfb31cab8204fc1125fd9b3b150a01d2ea780
Heriberto Cantú <es.beto@gmail.com>
2013-11-19 12:20:50 -0600

    Traducción forgot password

--- protected/views/site/forgotpassword.php
+++ protected/views/site/forgotpassword.php
@@ -1,8 +1,8 @@
 <div class="lw_right">
 <div class="inner_right">
 
-<h1>Forgot Password</h1>
-<p>Please enter your email address to receive new password.</p>
+<h1>Recuperar contraseña</h1>
+<p>Ingresa tu correo electrónico para recibir una nueva contraseña.</p>
 
 
 <?php $form=$this->beginWidget('CActiveForm', array(
@@ -21,13 +21,13 @@
 	</div>
 
 
-	
+
 	<table cellspacing="0" cellpadding="0" border="0" width="100%">
 	  <tr>
 		<td>
 		<div class="input-icon left">
 		<i class="icon-envelope"></i>
-            	<?php  echo $form->textField($model,'username', array('placeholder'=>'Email', 'style'=>'padding-left: 25px; width: 225px;')); ?><span class="new-status">&nbsp;</span>
+            	<?php  echo $form->textField($model,'username', array('placeholder'=>'Correo electrónico', 'style'=>'padding-left: 25px; width: 225px;')); ?><span class="new-status">&nbsp;</span>
 		<?php echo $form->error($model,'username'); ?>
 		</div>
 		</td>
@@ -35,8 +35,8 @@
 
         <tr>
             <td >
-		<?php echo CHtml::submitButton('Reset',array('align'=>'center','class'=>'submit','id'=>'login-button', 'style'=>'margin-right: 30px;')); ?>
-		<?php echo CHtml::link('Back',array('site/login'), array('style'=>"background: none repeat scroll 0 0 #E5E5E5;color: #333333;float: left;margin-left: 20px;padding: 10px 20px;
+		<?php echo CHtml::submitButton('Recuperar',array('align'=>'center','class'=>'submit','id'=>'login-button', 'style'=>'margin-right: 30px;')); ?>
+		<?php echo CHtml::link('Regresar',array('site/login'), array('style'=>"background: none repeat scroll 0 0 #E5E5E5;color: #333333;float: left; padding: 10px 20px;
 text-decoration: none;")); ?>
 	    </td>
         </tr>
