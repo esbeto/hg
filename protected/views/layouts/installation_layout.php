9b22dcddfe5c06ad827504d5f5b7ab612cc06dd3
Heriberto Cantú <es.beto@gmail.com>
2013-11-19 02:02:51 -0600

    traduccion instalacion

--- protected/views/layouts/installation_layout.php
+++ protected/views/layouts/installation_layout.php
@@ -3,16 +3,13 @@
     <head>
 	<meta charset="UTF-8" />
 	<meta name="language" content="en" />
-	<title>Edusec Installation</title>
+	<title>Instalación</title>
 <link href="<?php echo Yii::app()->baseUrl; ?>/css/installation.css" type="text/css" rel="stylesheet">
     </head>
    <div class="container">
    <div class="content" style="height: 565px; width: 615px;">
 	<div class="header">
-		<div class="app-logo">
-		   <img src="<?php echo Yii::app()->baseUrl; ?>/images/product.png" alt="logo" /> 
-		</div>
-		<h1 class="title">Installation Process</h1>
+		<h1 class="title">Instalación</h1>
 	</div>
 <?php
 echo $content;
@@ -20,4 +17,4 @@ echo $content;
 
 </div>
 </div></body>
-</html> 
+</html>
--- protected/views/site/redirect_login.php
+++ protected/views/site/redirect_login.php
@@ -1,13 +1,13 @@
 <br />
-<h2>Institute details are successfully saved. We have sent you login credential on your organization email.</h2>
+<h2>Los datos se guardaron con éxito. Te hemos enviado los detalles de login al email registrado.</h2>
 
-<p> For login : <?php echo CHtml::link('Click here..',Yii::app()->createUrl('site/login'));?>  </p>
+<p><?php echo CHtml::link('Click aquí',Yii::app()->createUrl('site/login'));?> para ingresar </p>
 
 
 <div style="color: green; font-size: 17px; font-weight: 700;">
 <?php //$userCount = User::model()->count();
 	$user = User::model()->findByPk(1);
-	echo 'Your UserName and Password is : '.$user->user_organization_email_id."\n";
+	echo 'Tu usuario y contraseña son: '.$user->user_organization_email_id."\n";
   //}
 ?>
 </div>
