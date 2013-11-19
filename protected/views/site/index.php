721f1091dec2dcee29722063d97b28ed4a5f4d4b
Heriberto Cantú <es.beto@gmail.com>
2013-11-19 01:38:56 -0600

    Traducción Index y agregar gitignore

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -3,28 +3,26 @@
     <head>
 	<meta charset="UTF-8" />
 	<meta name="language" content="en" />
-	<title>Edusec Installation</title>
+	<title>Instalación</title>
 <link href="<?php echo Yii::app()->baseUrl; ?>/css/installation.css" type="text/css" rel="stylesheet">
     </head>
    <div class="container">
    <div class="content">
 	<div class="header">
-		<div class="app-logo">
-		   <img src="<?php echo Yii::app()->baseUrl; ?>/images/product.png" alt="logo" /> 
-		</div>
+        <h1 class="title">Instalación</h1>
 	</div>
 
 
-<?php 
+<?php
 
 	$sql = file_get_contents(Yii::app()->basePath.'/data/db.sql');
 	$command=Yii::app()->db->createCommand($sql);
-	$command->execute();	
+	$command->execute();
 
-echo '<span style="float: left; margin-top:10px; color: green; font-size: 15px;">Database configuratin process completed. Please '.CHtml::link('click here', 'createOrg').' to create an Institute.</span>'; 
+echo '<span style="float: left; margin-top:10px; color: green; font-size: 15px;">Se ha finalizado el proceso de configuración de la base de datos. <br> Favor de hacer click '.CHtml::link('aquí', 'createOrg').' para registrar un Instituto.</span>';
 
 ?>
 
 </div>
 </div></body>
-</html> 
+</html>
